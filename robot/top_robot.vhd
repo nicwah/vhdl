63cb3baf8b2c26232fdd2893de22445555fc18b8
froste242 <bt.frostemark@gmail.com>
2018-04-27 14:14:38 +0200

    Add components in top module

--- robot/top_robot.vhd
+++ robot/top_robot.vhd
@@ -55,31 +55,82 @@ alias bluetooth_in : std_logic is pio(86);
 
 component timer
     port(
+        
     );
-end component;
 
 component clock
     port(
+        sysclk : in std_logic;
     );
 end component;
 
 component motor
-    port(
-    );
+    Port ( speed : in  STD_LOGIC_VECTOR (2 downto 0);
+           dir : in  STD_LOGIC;
+           motor_ena : out  STD_LOGIC;
+           motor_enb : out  STD_LOGIC;
+           motor_in1 : out  STD_LOGIC;
+           motor_in2 : out  STD_LOGIC;
+           motor_in3 : out  STD_LOGIC;
+           motor_in4 : out  STD_LOGIC);
+end component;
+
+component driver
+    Port ( alarm : in  STD_LOGIC;
+           speed : out  STD_LOGIC_VECTOR (2 downto 0);
+           direction : out  STD_LOGIC);
 end component;
 
 begin
 
-    TimerInst : timer
-        port map (
-        );
-        
+    signal speed : STD_LOGIC_VECTOR (2 downto 0);
+
     ClockInst : clock
         port map (
+            mclk => sysclk;
         );
         
     MotorInst : motor
         port map (
+           speed => speed;
+           dir => dir;
+           motor_ena => motor_ena;
+           motor_enb => motor_enb;
+           motor_in1 => motor_in1;
+           motor_in2 => motor_in2;
+           motor_in3 => motor_in3;
+           motor_in4 => motor_in4;
+        );
+
+    DriverInst : driver
+        port map (
+            alarm => driver_alarm;
+            speed => speed;
+            direction => dir;
+        );
+
+    DriverTimerInst : timer
+        port map (
+            wait_time => driver_wait_time;
+            alarm => driver_alarm;
+            enable => driver_enable;
+            ms_clock => ms_clock;
+        );
+
+    MotorTimer1Inst : timer
+        port map (
+            wait_time => motor_wait_time_1;
+            alarm => motor_alarm_1;
+            enable => motor_enable_1;
+            ms_clock => ms_clock;
+        );
+
+    MotorTimer2Inst : timer
+        port map (
+            wait_time => motor_wait_time_2;
+            alarm => motor_alarm_2;
+            enable => motor_enable_2;
+            ms_clock => ms_clock;
         );
 
 end Behavioral;
